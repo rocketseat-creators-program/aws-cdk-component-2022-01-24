1317f38cbd6bd6ed9292c244da378e2a72c48920
Bruno Russi Lautenschlager <brunoxd13@gmail.com>
2022-01-24 09:24:24 -0300

    test: create tests for component

--- test/hello.test.ts
+++ test/hello.test.ts
@@ -1,5 +1,59 @@
-import { Hello } from '../src';
+import { aws_lambda as lambda, App, Stack } from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import { RocketComponent } from '../src';
 
-test('hello', () => {
-  expect(new Hello().sayHello()).toBe('hello, world!');
+test('Create full component', () => {
+  // Given
+  const app = new App();
+  const stack = new Stack(app, 'stack-test');
+
+  // When
+  new RocketComponent(stack, 'RocketeatComponent', {
+    stackPrefix: 'ExpertsClub',
+    lambdaCode: lambda.AssetCode.fromAsset('lambda'),
+    createDynamoDbTable: true,
+  });
+
+  // Then
+  const template = Template.fromStack(stack);
+
+  template.resourceCountIs('AWS::Lambda::Function', 1);
+  template.resourceCountIs('AWS::DynamoDB::Table', 1);
+  template.resourceCountIs('AWS::ApiGateway::RestApi', 1);
+});
+
+test('Create component without dynamo', () => {
+  // Given
+  const app = new App();
+  const stack = new Stack(app, 'stack-test');
+
+  // When
+  new RocketComponent(stack, 'RocketeatComponent', {
+    stackPrefix: 'ExpertsClub',
+    lambdaCode: lambda.AssetCode.fromAsset('lambda'),
+    createDynamoDbTable: false,
+  });
+
+  // Then
+  const template = Template.fromStack(stack);
+
+  template.resourceCountIs('AWS::Lambda::Function', 1);
+  template.resourceCountIs('AWS::DynamoDB::Table', 0);
+  template.resourceCountIs('AWS::ApiGateway::RestApi', 1);
+});
+
+test('Create component default', () => {
+  // Given
+  const app = new App();
+  const stack = new Stack(app, 'stack-test');
+
+  // When
+  new RocketComponent(stack, 'RocketeatComponent');
+
+  // Then
+  const template = Template.fromStack(stack);
+
+  template.resourceCountIs('AWS::Lambda::Function', 1);
+  template.resourceCountIs('AWS::DynamoDB::Table', 1);
+  template.resourceCountIs('AWS::ApiGateway::RestApi', 1);
 });
