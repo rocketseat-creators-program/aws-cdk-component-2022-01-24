4ab9df7c9ddc680e6d59cb1bb6af128af30b6cb8
Bruno Russi Lautenschlager <brunoxd13@gmail.com>
2022-01-24 08:16:49 -0300

    chore: project created with projen

---
+++ .projenrc.js
@@ -0,0 +1,15 @@
+const { awscdk } = require('projen');
+const project = new awscdk.AwsCdkConstructLibrary({
+  author: 'Bruno Russi Lautenschlager',
+  authorAddress: 'brunoxd13@gmail.com',
+  cdkVersion: '2.1.0',
+  defaultReleaseBranch: 'main',
+  name: 'rocketseat-component',
+  repositoryUrl: 'https://github.com/brunoxd13/rocketseat-component.git',
+
+  // deps: [],                /* Runtime dependencies of this module. */
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // devDeps: [],             /* Build dependencies for this module. */
+  // packageName: undefined,  /* The "name" in package.json. */
+});
+project.synth();
